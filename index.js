49ebea36241868e974f2ba1df0d4a4849541c5eb
Chris <stac1221@gmail.com>
2018-09-01 15:54:50 -0500

    setting up middleware

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express =     require('express'),
-      app =         express(),
-      bodyParser =  require('body-parser'),
-      port =        process.env.PORT || 8080,
-      slack =       require('./slack.js');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
+const slack = require('./slack.js');
 
 app.set('port', (process.env.PORT || 8080));
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-	res.send('hello!');
+// app.post('/', function(req, res){
+//   res.send(req.body);
+// 	res.sendStatus(200);
+// });
+
+const requestTime = (req, res, next) => {
+  req.requestTime = new Date();
+  next();
+}
+
+app.use(requestTime);
+
+app.get('/', (req, res) => {
+  let responseText = '<b>Requested at: </b>' + req.requestTime;
+  res.send(responseText);
 });
 
-app.listen(port, function() {
+// app.get('/', function(req, res) {
+// 	res.send('app.get');
+// });
+
+app.listen(port, () => {
   console.log('running on http://localhost:' + port);
 });
