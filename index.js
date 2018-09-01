9a58fa44dbbd11abeefd7f5171a3a2d49c370ac5
Chris <stac1221@gmail.com>
2018-09-01 08:38:44 -0500

    updates for heroku

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const express =     require('express'),
       bodyParser =  require('body-parser'),
       port =        process.env.PORT || 8080,
       slack =       require('./slack.js');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.send('hello!');
+});
+
+app.listen(port, function() {
+  console.log('running on http://localhost:' + port);
+});
