e027ad71f42e059fc4e7d98d2e3d7217a5c66892
Chris <stac1221@gmail.com>
2018-09-03 23:51:22 -0500

    cleaned up project structure and code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
-const slack = require('./slack.js');
 const moment = require('moment');
 
 app.set('port', (process.env.PORT || 8080));
@@ -10,18 +9,7 @@ app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 
-// const requestTime = (req, res, next) => {
-//   req.requestTime = new Date();
-//   next();
-// }
-
 app.post('/', (req, res) => {
-
-  // let requestTime = moment(new Date()).format('H:mm')
-  // let timeString = req.body.text;
-  // let momentObject = moment(timeString, 'H:mm a');
-  // let momentString = momentObject.format('H:mm');
-  // let countdown = moment(momentString, 'H:mm').fromNow('mm');
 
   let date = moment(new Date()).format('YYYY-MM-DD');
   let timeString = req.body.text;
@@ -64,52 +52,8 @@ app.post('/', (req, res) => {
   res.send(response);
 });
 
-// app.use(requestTime);
-
 app.get('/', (req, res) => {
-
-  let date = moment(new Date()).format('YYYY-MM-DD');
-  let timeString = '11:20PM';
-  let momentObject = moment(timeString, 'H:mm a');
-  let momentString = date + ' ' + momentObject.format('H:mm');
-
-  const msToTime = (duration) => {
-    let milliseconds = parseInt((duration % 1000) / 100),
-      seconds = parseInt((duration / 1000) % 60),
-      minutes = parseInt((duration / (1000 * 60)) % 60),
-      hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-  
-      console.log(seconds + ' seconds');
-      console.log(minutes + ' minutes');
-      console.log(hours + ' hours');
-  
-    hours = (hours < 10) ? hours : hours;
-    minutes = (minutes < 10) ? + minutes : minutes;
-    seconds = (seconds < 10) ? + seconds : seconds;
-  
-    if (hours === 0) {
-      return minutes + 'm ' + seconds + 's';
-      console.log('hours = 0');
-    } else {
-      return hours + 'h ' + minutes + 'm ' + seconds + 's';
-    }
-  }
-
-  const d1 = new Date();
-  const d2 = new Date(momentString);
-  const diff = Math.abs(d1 - d2);
-  console.log('diff: ', diff);
-
-  let countdown = timeString + ' is in ' + msToTime(diff);
-  if (isNaN(diff)) {
-    countdown = 'Please type time in this format— "12:00PM"';
-  }
-
-  let responseText = {
-    'response_type': 'in_channel',
-    'text': countdown
-  }
-  res.send(responseText.text);  
+  res.send('slack_clock-countdown');  
   res.sendStatus(200);
 });
 
--- slack.js
+++
@@ -1,1 +0,0 @@
-console.log('slack.js');
